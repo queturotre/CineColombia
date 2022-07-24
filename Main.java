0687b7237c4e54a927d14bda24fc3425549de2cb
Ricardo Miguel García <74616027+queturotre@users.noreply.github.com>
2022-07-24 15:13:11 -0500

    Update Main.java
    Somehow main class was not added

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main (String [] args){
         Scanner miguel = new Scanner(System.in); // Lector
         char[][] a = new char [9][9]; // La sala de cine es una matriz.
         int b = 0; // Filas.
